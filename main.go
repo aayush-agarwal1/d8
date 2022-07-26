92c4da427c457aa14f38d4734e94a04cd4db00e3
aayush.agarwal1 <aayush.agarwal1@swiggy.in>
2022-07-26 15:36:25 +0530

    added post method

--- main.go
+++ main.go
@@ -7,17 +7,29 @@ import (
 )
 
 type user struct {
-	Name string
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 func myHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		dummyUser := user{"aayush"}
-		json.NewEncoder(w).Encode(dummyUser)
+		dummyUser := user{"aayush", 21}
+		err := json.NewEncoder(w).Encode(dummyUser)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+	} else if r.Method == "POST" {
+		var dummyUser user
+		err := json.NewDecoder(r.Body).Decode((&dummyUser))
+		fmt.Println(dummyUser)
+		if err != nil {
+			http.Error(w, "Incorrect User struct format", http.StatusBadRequest)
+		}
+	} else {
+		http.Error(w, "Unsupported Method", http.StatusBadRequest)
 	}
 }
 func main() {
-	fmt.Printf("Hello World 2")
 	http.HandleFunc("/", myHandler)
 	http.ListenAndServe("127.0.0.1:8080", nil)
 }
